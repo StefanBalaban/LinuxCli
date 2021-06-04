210641fc346a752ecac636a4df02963071ae3182
stefan.balaban@klika.ba <Stefan>
2021-06-04 19:50:51 +0200

    Add project files.

---
+++ LinuxCli/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LinuxCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "mkdir $HOME/newDir";
+            string result = "";
+            using (System.Diagnostics.Process proc = new System.Diagnostics.Process())
+            {
+                proc.StartInfo.FileName = "/bin/bash";
+                proc.StartInfo.Arguments = "-c \" " + command + " \"";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.Start();
+
+                result += proc.StandardOutput.ReadToEnd();
+                result += proc.StandardError.ReadToEnd();
+
+                proc.WaitForExit();
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
